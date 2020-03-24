543b0e32638c824be8846d522c772d18302e8564
Kirill Khoroshilov <mr.hokid@gmail.com>
2020-03-24 18:39:15 +0300

    feat: better frame switching algorithm

--- src/engines/puppeteer/AnimationCapture.ts
+++ src/engines/puppeteer/AnimationCapture.ts
@@ -1,25 +1,22 @@
 import {Page} from './Page';
 import {EngineFrames} from '../../core/engine';
 import {Logger} from '../../core/logger';
+import {AnimationCaptureState} from './AnimationCaptureState';
 
 export class AnimationCapture implements EngineFrames {
-    readonly totalFrames: number = 0;
-    currentFrame: number = 0;
+    private state: AnimationCaptureState;
 
-    private readonly fps: number = 30;
-    private readonly delay: number = 0;
-    private readonly duration: number = 0;
-    private readonly frameDuration: number = 0;
-    private readonly fFrameDuration: number = 0;
-    private readonly offsetPayload: number = 0;
-    private isAnimationPaused: boolean = false;
-    private lastStartAnimationCallTime: number = 0;
-    private lastStopAnimationCallTime: number = 0;
-    private lastStartStopAnimationCallTime: number = 0;
-    private lastWait: number = 0;
-    private balance: number = 0;
-    private needDelay: boolean = true;
-    private isReady: boolean = false;
+    get currentFrame(): number {
+        return this.state.currentFrame;
+    }
+
+    get totalFrames(): number {
+        return this.state.totalFrames;
+    }
+
+    get isReady(): boolean {
+        return this.state.isReady;
+    }
 
     constructor(
         private page: Page,
@@ -30,172 +27,115 @@ export class AnimationCapture implements EngineFrames {
             fps?: number
         }
     ) {
-        this.duration = parameters.duration;
+        this.state = new AnimationCaptureState(parameters);
 
-        if (parameters.fps) {
-            this.fps = parameters.fps
-        }
-
-        if (parameters.delay) {
-            this.delay = parameters.delay
-        }
-
-        if (this.delay === 0) {
-            this.needDelay = false;
-        }
-
-        this.frameDuration = Math.floor(1000 / this.fps);
-        this.fFrameDuration = (1000 / this.fps) % 1;
-        this.totalFrames = Math.floor(this.duration / 1000 * this.fps);
-        this.offsetPayload = Math.ceil(this.frameDuration * 0.1);
-
-        logger.debug(this.constructor.name, 'initial parameters', {
-            delay: this.delay,
-            fps: this.fps,
-            duration: this.duration,
-            frameDuration: this.frameDuration,
-            fFrameDuration: this.fFrameDuration,
-            totalFrames: this.totalFrames,
-            offsetPayload: this.offsetPayload
-        });
+        logger.debug(this.constructor.name, 'initial state', this.state.clone());
     }
 
     async getReady() {
-        if (!this.isReady) {
+        if (!this.state.isReady) {
             await this.stopAnimation();
-            this.isReady = true;
+            this.state.isReady = true;
             this.logger.debug(this.constructor.name, 'ready for use');
         }
     }
 
     hasNextFrame(): boolean {
-        return this.totalFrames > this.currentFrame;
+        return this.state.totalFrames > this.state.currentFrame;
     }
 
     async nextFrame(): Promise<boolean> {
-        if (!this.isReady) {
+        const state = this.state;
+
+        if (!state.isReady) {
             throw new Error('AnimationCapture is not ready');
         }
 
-        let add: number = 0;
-
-        this.logger.debug(this.constructor.name, 'preparing next frame');
-
-
-        if (this.totalFrames === 0) {
-            this.logger.debug(this.constructor.name, 'total frames is zero');
+        if (state.totalFrames === 0) {
+            this.logger.debug(this.constructor.name, 'total frames is zero', this.state.clone());
             return false;
         }
 
         if (!this.hasNextFrame()) {
-            this.logger.debug(this.constructor.name, 'no more frames');
+            this.logger.debug(this.constructor.name, 'no more frames', this.state.clone());
             return false;
         }
 
-        ++this.currentFrame;
+        ++state.currentFrame;
 
-        this.logger.debug(this.constructor.name, 'got next frame number', this.currentFrame);
+        this.logger.debug(this.constructor.name, 'preparing next frame', this.state.clone());
 
-        const frameRate = this.frameDuration + this.fFrameDuration;
-
-        this.logger.debug(this.constructor.name, 'got next frame rate', frameRate);
-
-        if (this.needDelay || this.currentFrame === 1) {
-            const delay = this.delay > 0
-                ? (Math.floor(this.delay / frameRate)) + (Math.ceil(frameRate / 2) * Math.ceil(this.delay % frameRate))
-                : Math.ceil(frameRate / 2);
-
-            this.logger.debug(this.constructor.name, 'got next frame delay', delay);
+        if (state.currentFrame === 1) {
+            state.updateWait();
 
             await this.startAnimation();
-            await this.wait(delay);
+            await this.wait(state.wait);
             await this.stopAnimation();
 
-            this.needDelay = false;
-            this.addBalance(this.lastStartStopAnimationCallTime + (this.lastWait - delay));
-        }
+            state.updateLag();
 
-        // we already on the first frame, no need to do start/stop animation next
-        if (this.currentFrame === 1) {
-            this.logger.debug(this.constructor.name, 'next frame prepared');
+            this.logger.debug(this.constructor.name, 'delay applied', this.state.clone());
+            this.logger.debug(this.constructor.name, 'next frame prepared', this.state.clone());
+
+            // we already on the first frame, no need to do start/stop animation again
             return true;
         }
 
-        this.addBalance(-this.fFrameDuration);
-
-        if (this.balance <= 0 || this.balance > this.offsetPayload) {
-            add = Math.floor(this.balance * -1) + this.offsetPayload;
-            this.addBalance(add);
-
-            this.logger.debug(this.constructor.name, 'got next frame addition waiting time', add);
-        }
-
-        this.logger.debug(this.constructor.name, 'got next frame animation waiting time', this.frameDuration + add);
+        state.increaseCurrentFramesFractionsParts();
+        state.updateWait();
 
         await this.startAnimation();
-        await this.wait(this.frameDuration + add);
+        await this.wait(state.wait);
         await this.stopAnimation();
 
-        this.addBalance(this.lastStartStopAnimationCallTime + this.lastWait - (this.frameDuration + add));
+        state.updateLag();
 
-        this.logger.debug(this.constructor.name, 'next frame prepared');
+        this.logger.debug(this.constructor.name, 'next frame prepared', this.state.clone());
 
         return true;
     }
 
     async reset(): Promise<void> {
-        this.balance = 0;
-        this.currentFrame = 0;
     }
 
     private wait(ms: number): Promise<void> {
         return new Promise(done => {
-            this.logger.debug(this.constructor.name, 'waiting...');
+            this.logger.debug(this.constructor.name, 'waiting...', this.state.clone());
             const start = Date.now();
             setTimeout(() => {
                 const end = Date.now();
-                this.lastWait = end - start;
-                this.logger.debug(this.constructor.name, 'done waiting', this.lastWait);
+                this.state.lastWaitCallTime = end - start;
+                this.logger.debug(this.constructor.name, 'waiting done', this.state.clone());
                 done();
             }, ms);
         });
     }
 
     private async startAnimation() {
-        if (this.isAnimationPaused) {
-            this.logger.debug(this.constructor.name, 'starting animation...');
+        if (this.state.isAnimationPaused) {
+            this.logger.debug(this.constructor.name, 'starting animation...', this.state.clone());
             const start = Date.now();
             await (<any>this.page.getPage())._client.send('Animation.setPlaybackRate', {playbackRate: 1});
             const end = Date.now();
-            this.lastStartAnimationCallTime = end - start;
-            this.isAnimationPaused = false;
-            this.logger.debug(this.constructor.name, 'animation started', this.lastStartAnimationCallTime);
+            this.state.lastStartAnimationCallTime = end - start;
+            this.state.isAnimationPaused = false;
+            this.logger.debug(this.constructor.name, 'animation started', this.state.clone());
         }
     }
 
     private async stopAnimation() {
-        if (!this.isAnimationPaused) {
-            this.logger.debug(this.constructor.name, 'stopping animation...');
+        if (!this.state.isAnimationPaused) {
+            this.logger.debug(this.constructor.name, 'stopping animation...', this.state.clone());
             const start = Date.now();
             await (<any>this.page.getPage())._client.send('Animation.setPlaybackRate', {playbackRate: 0});
             const end = Date.now();
 
-            if (this.isReady) {
-                this.lastStopAnimationCallTime = end - start;
-                this.updateLastStartStopAnimationTime();
+            if (this.state.isReady) {
+                this.state.lastStopAnimationCallTime = end - start;
             }
-            this.logger.debug(this.constructor.name, 'animation stop', this.lastStopAnimationCallTime);
-            this.isAnimationPaused = true;
+
+            this.state.isAnimationPaused = true;
+            this.logger.debug(this.constructor.name, 'animation stop', this.state.clone());
         }
-    }
-
-    private addBalance(value: number) {
-        this.balance += value;
-        this.logger.debug(this.constructor.name, 'balance changed', this.balance);
-    }
-
-    private updateLastStartStopAnimationTime() {
-        this.lastStartStopAnimationCallTime = (this.lastStartAnimationCallTime + this.lastStopAnimationCallTime) * 0.5;
-        this.logger.debug(this.constructor.name, 'last start/stop animation call time', this.lastStartStopAnimationCallTime);
     }
 }
---
+++ src/engines/puppeteer/AnimationCaptureState.ts
@@ -0,0 +1,112 @@
+export class AnimationCaptureState {
+    readonly totalFrames: number = 0;
+    currentFrame: number = 0;
+
+    isAnimationPaused: boolean = false;
+    isReady: boolean = false;
+
+    readonly fps: number = 30;
+    readonly delay: number = 0;
+    readonly duration: number = 0;
+    readonly frameDuration: number = 0;
+    readonly frameDurationWholePart: number = 0;
+    readonly frameDurationFractionPart: number = 0;
+
+    lastLag: number = 0;
+    lastFrameSwitchLag: number = 0;
+    lastWaitLag: number = 0;
+    lastStartAnimationCallTime: number = 0;
+    lastStopAnimationCallTime: number = 0;
+    lastWaitCallTime: number = 0;
+    wait: number = 0;
+    currentFramesFractionsParts: number = 0;
+
+    constructor(
+        parameters: {
+            duration: number,
+            delay?: number,
+            fps?: number
+        }
+    ) {
+        this.duration = parameters.duration;
+
+        if (parameters.fps) {
+            this.fps = parameters.fps
+        }
+
+        if (parameters.delay) {
+            this.delay = parameters.delay
+        }
+
+        this.frameDuration = 1000 / this.fps;
+        this.frameDurationWholePart = Math.floor(this.frameDuration);
+        this.frameDurationFractionPart = this.frameDuration % 1;
+        this.totalFrames = Math.floor(this.duration / 1000 * this.fps);
+    }
+
+    updateWait() {
+        if (this.currentFrame === 1) {
+            this.wait = this.getDelay();
+            return;
+        }
+
+        const fractionPartGarbage = this.collectCurrentFramesFractionsPartsGarbage();
+
+        this.wait = this.frameDurationWholePart - this.lastLag + fractionPartGarbage;
+    }
+
+    private getDelay(): number {
+        if (this.currentFrame === 1) {
+            const half = Math.ceil(this.frameDuration / 2);
+
+            if (this.delay === 0) {
+                return half;
+            }
+
+            const delayWhole = Math.floor(this.delay / this.frameDuration);
+            const takeHalf = Math.ceil(this.delay % this.frameDuration);
+
+            return delayWhole + half * takeHalf;
+        }
+
+        return 0;
+    }
+
+    private collectCurrentFramesFractionsPartsGarbage(): number {
+        if (this.currentFramesFractionsParts >= 1) {
+            this.currentFramesFractionsParts--;
+            return 1;
+        }
+        return 0;
+    }
+
+    increaseCurrentFramesFractionsParts() {
+        this.currentFramesFractionsParts += this.frameDurationFractionPart;
+    }
+
+    updateLag() {
+        this.updateFrameSwitchLag();
+        this.updateWaitLag();
+        this.lastLag = this.lastWaitLag + this.lastFrameSwitchLag;
+    }
+
+    updateFrameSwitchLag() {
+        this.lastFrameSwitchLag = this.lastStartAnimationCallTime + this.lastStopAnimationCallTime;
+    }
+
+    updateWaitLag() {
+        this.lastWaitLag = this.lastWaitCallTime - this.wait;
+    }
+
+    clone(): AnimationCaptureState {
+        const inst = new AnimationCaptureState({
+            fps: this.fps,
+            delay: this.delay,
+            duration: this.duration
+        });
+
+        Object.assign(inst, this);
+
+        return inst;
+    }
+}
